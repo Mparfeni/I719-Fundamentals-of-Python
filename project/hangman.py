be8b67f685651efe918df0fdc09f63d4d0079226
Mark Parfeniuk <mparfeni@itcollege.ee>
2017-03-13 23:31:05 +0200

    Divided code into 2 parts
    Divided code into main file with methods and file for dictionary.

--- project/hangman.py
+++ project/hangman.py
@@ -1,5 +1,5 @@
 import random
-words = ["continental", "hangman", "chairs", "backpack", "bodywash", "clothing", "computer", "python", "program", "glasses", "sweatshirt", "sweatpants", "mattress", "friends", "clocks", "biology", "algebra", "suitcase", "knives", "ninjas", "shampoo", "ray", "daisy", "assassination", "hopscotch", "beads", "chargeable", "amongst", "chameleon", "calling", "aerial", "accommodation", "smuggler", "compact", "swollen", "elsewhere", "art", "absurd", "trial", "royalty", "loser", "opposition", "bitter", "downward", "computation", "empire", "hideout", "expression", "inverse", "indecent", "address", "creative", "heavyhearted", "conflict", "toy", "high", "flicker"]
+import dictionary
 def choose_a_word():
-	word_position = random.randint(0, len(words) - 1)
-	return words[word_position]
+	word_position = random.randint(0, len(dictionary.words) - 1)
+	return dictionary.words[word_position]
