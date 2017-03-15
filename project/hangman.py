037c966efb47703f1bdad264df27758109cbb46b
Mark Parfeniuk <mparfeni@itcollege.ee>
2017-03-15 12:38:24 +0200

    Added main method

--- project/hangman.py
+++ project/hangman.py
@@ -1,5 +1,21 @@
 import random
 import dictionary
+
+
 def choose_a_word():
 	word_position = random.randint(0, len(dictionary.words) - 1)
 	return dictionary.words[word_position]
+
+
+def game():
+    our_word = choose_a_word()
+    while True:
+        guess = get_guess(our_word)
+        if guessing(guess, our_word):
+            print("You win!")
+            break
+        if lives_left == 0:
+            print("Wasted!")
+            print("The word was: " + our_word)
+            break
+
